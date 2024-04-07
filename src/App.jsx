b3a8f41b3763ee1c7b4c605aa3333d174e7b8c4f
yunamoon <tpwkao0051@gmail.com>
2024-04-07 14:32:24 +0900

    api complete

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,6 @@ import { CallGPT } from './api/gpt';
 import UserForm from './components/UserForm';
 import styled from 'styled-components';
 
-
 function App() {
   const [data , setData ] = useState('');
   const [isLoading , setIsLoading] = useState(false);
@@ -15,8 +14,9 @@ function App() {
       const message = await CallGPT({
         prompt: `${userInput}`,
       });
-      setData(message);
-    } catch (error) {
+      setData(JSON.parse(message));
+    } catch (error) { 
+      /* empty */ 
     } finally {
       setIsLoading(false);
     }
@@ -26,11 +26,14 @@ function App() {
     handleClickAPICall(userInput);
   };
   
+  console.log(">>data" , data)
  
   return (
     <AppContainer>
+      <AppTitle>
+        오늘의 위로 🌕
+      </AppTitle>
     <div>메세지 왔다 : {data}</div>
-
     <UserForm    
     isLoading={isLoading}
     onSubmit={handleSubmit}/>
@@ -40,11 +43,19 @@ function App() {
 
 export default App;
 
-const AppContainer = styled.div`
-padding: 20px;
-display : felx;
-flex-direction : colum;
-max-width : 720px;
-width : 100%;
-mrgin : 0 auto;
+const AppContainer = styled.div `
+  padding: 20px;
+  display : flex;
+  flex-direction : column;
+  max-width: 720px;
+  width : 100%;
+  margin: 0 auto;
+`;
+
+const AppTitle = styled.div`
+  width : 100%;
+  font-weight : 400;
+  font-size : 35px;
+  text-align : center;
+  font-family: "Noto Serif KR";
 `;
