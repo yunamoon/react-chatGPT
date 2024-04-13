a974d6cda67375ebaf8c47320a405c11e78bb9c7
yunamoon <tpwkao0051@gmail.com>
2024-04-13 19:06:30 +0900

    update diary

--- src/App.jsx
+++ src/App.jsx
@@ -1,61 +1,20 @@
-import { useState } from 'react';
+import { Routes, Route} from 'react-router-dom';
 import './App.css'
-import { CallGPT } from './api/gpt';
-import UserForm from './components/UserForm';
-import styled from 'styled-components';
+import Home from './pages/Home';
+import Form from './pages/Form';
+import Diary from './pages/Diary';
+import Notfound from './pages/Notfound';
 
 function App() {
-  const [data , setData ] = useState('');
-  const [isLoading , setIsLoading] = useState(false);
-
-  const handleClickAPICall = async (userInput) => {
-    try {
-      setIsLoading(true);
-      const message = await CallGPT({
-        prompt: `${userInput}`,
-      });
-      setData(JSON.parse(message));
-    } catch (error) { 
-      /* empty */ 
-    } finally {
-      setIsLoading(false);
-    }
-  };
-
-  const handleSubmit = (userInput) => {
-    handleClickAPICall(userInput);
-  };
-  
-  console.log(">>data" , data)
- 
   return (
-    <AppContainer>
-      <AppTitle>
-        오늘의 위로 🌕
-      </AppTitle>
-    <div>메세지 왔다 : {data}</div>
-    <UserForm    
-    isLoading={isLoading}
-    onSubmit={handleSubmit}/>
-    </AppContainer>
-  )
+    <Routes>
+      <Route path='/' element={<Home/>} />
+      <Route path='/create' element={<Form/>}/>
+      <Route path='/diary' element={<Diary/>}/>
+      <Route path='*' element={<Notfound/>}/>
+    </Routes>
+  );
 }
 
 export default App;
 
-const AppContainer = styled.div `
-  padding: 20px;
-  display : flex;
-  flex-direction : column;
-  max-width: 720px;
-  width : 100%;
-  margin: 0 auto;
-`;
-
-const AppTitle = styled.div`
-  width : 100%;
-  font-weight : 400;
-  font-size : 35px;
-  text-align : center;
-  font-family: "Noto Serif KR";
-`;
---
+++ src/pages/Form.jsx
@@ -0,0 +1,59 @@
+import { useState } from 'react';
+import { CallGPT } from '../api/gpt';
+import styled from 'styled-components';
+import UserForm from '../components/UserForm';
+
+const Form = () => {
+
+    const [data , setData ] = useState('');
+    const [isLoading , setIsLoading] = useState(false);
+  
+    const handleClickAPICall = async (userInput) => {
+      try {
+        setIsLoading(true);
+        const message = await CallGPT({
+          prompt: `${userInput}`,
+        });
+        setData(JSON.parse(message));
+      } catch (error) { 
+        /* empty */ 
+      } finally {
+        setIsLoading(false);
+      }
+    };
+  
+    const handleSubmit = (userInput) => {
+      handleClickAPICall(userInput);
+    };
+
+  return (
+    <AppContainer>
+      <AppTitle>
+        오늘의 위로 🌕
+      </AppTitle>
+    <div>메세지 왔다 : {data}</div>
+    <UserForm    
+    isLoading={isLoading}
+    onSubmit={handleSubmit}/>
+    </AppContainer>
+  )
+}
+
+export default Form
+
+const AppContainer = styled.div `
+  padding: 20px;
+  display : flex;
+  flex-direction : column;
+  max-width: 720px;
+  width : 100%;
+  margin: 0 auto;
+`;
+
+const AppTitle = styled.div`
+  width : 100%;
+  font-weight : 400;
+  font-size : 35px;
+  text-align : center;
+  font-family: "Noto Serif KR";
+`;
