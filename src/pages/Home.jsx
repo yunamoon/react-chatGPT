d825b544d44ff9cdab03e8f836757c0de0e4a55d
yunamoon <tpwkao0051@gmail.com>
2024-04-24 23:53:46 +0900

    date update function...ing

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,16 +1,24 @@
-
-import React from 'react'
+import React, { useState } from 'react'
 import Header from '../components/header/Header';
 import Button from '../components/button/Button';
 import Diary from '../components/diary/Diary';
 
 const Home = () => {
+
+  const [pivotDate , setPivotDate] = useState(new Date());
+  const onLastMonth = () => {
+    setPivotDate(new Date(pivotDate.getFullYear(), pivotDate.getMonth() -1));
+  };
+  const onNextMonth = () => {
+    setPivotDate(new Date(pivotDate.getFullYear(), pivotDate.getMonth() +1));
+  };
+
   return (
     <>
     <Header 
-    title={'2024년 04월'}
-    leftChild={<Button text={'<'}/>}
-    rightChild={<Button text={'>'}/>}/>
+    title={`${pivotDate.getFullYear()}년 ${pivotDate.getMonth() +1}월`}
+    leftChild={<Button onClick={onLastMonth} text={'<'}/>}
+    rightChild={<Button onClick={onNextMonth} text={'>'}/>}/>
     <Diary/>
     </>
   )
